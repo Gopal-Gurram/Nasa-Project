916e07b885cc21c1c7195bd04ffc7c40fb9f955f
Gopal-Gurram <gopalgurram1@gmail.com>
2023-02-28 22:43:14 +0530

    initial commit

---
+++ server/src/app.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const express = require("express");
+const planetRouter = require("./routes/planets/planets.router");
+const cors = require("cors");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(planetRouter);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
+
+module.exports = app;
